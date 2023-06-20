cdfda897145876dd39a5d847a3c6833e3ee17a61
Bravi Dang <hcmminer@gmail.com>
2023-06-21 02:04:34 +0700

    add sw

--- src/main/java/com/ecom/shop/config/SwaggerConfig.java
+++ src/main/java/com/ecom/shop/config/SwaggerConfig.java
@@ -2,25 +2,27 @@ package com.ecom.shop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(SpringDataRestConfiguration.class)
 public class SwaggerConfig {
-
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(getApiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.educative.ecommerce"))
+                .apis(RequestHandlerSelectors.basePackage("com.ecom.shop"))
                 .paths(PathSelectors.any())
                 .build();
     }
