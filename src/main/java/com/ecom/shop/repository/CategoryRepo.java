292835c130a7098b0964b45ed97f109363ce6b52
Bravi Dang <hcmminer@gmail.com>
2023-06-21 01:26:33 +0700

    down to 2.5.4

---
+++ src/main/java/com/ecom/shop/repository/CategoryRepo.java
@@ -0,0 +1,10 @@
+package com.ecom.shop.repository;
+
+import com.ecom.shop.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepo extends JpaRepository<Category, Integer> {
+
+}
---
+++ src/main/java/com/ecom/shop/service/CategoryService.java
@@ -0,0 +1,24 @@
+package com.ecom.shop.service;
+
+import com.ecom.shop.model.Category;
+import com.ecom.shop.repository.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    CategoryRepo categoryRepo;
+
+    public void createCategory(Category category) {
+        categoryRepo.save(category);
+    }
+
+    public List<Category> listCategory() {
+        return categoryRepo.findAll();
+    }
+
+}
